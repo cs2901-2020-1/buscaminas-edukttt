3c2936344fa04c106c3043a5918d17472c51685e
StephanoWurttele <stephano.wurttele@utec.edu.pe>
2020-06-04 12:31:11 -0500

    D: x2

---
+++ Buscaminas/src/main/java/Buscaminas.java
@@ -0,0 +1,17 @@
+public class Buscaminas {
+    public static void fill(String [][] board){
+
+        for (int a = 0; a < 10; a++) {
+            for (int j= 0; j < 10; j++) {
+                if(Math.random() < 2)
+                    board[a][j] = "*";
+            }
+        }
+    }
+
+    public static void run(String [][] board, String [][] displayedBoard){
+        fill(board);
+        Display.display(board);
+        Display.display(displayedBoard);
+    }
+}
