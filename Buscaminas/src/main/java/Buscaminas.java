cb268047ff62e133e37329d033f0ec02f846de97
Diego Linares <33405883+DiegoELT@users.noreply.github.com>
2020-06-04 12:44:00 -0500

    Update Buscaminas.java

--- Buscaminas/src/main/java/Buscaminas.java
+++ Buscaminas/src/main/java/Buscaminas.java
@@ -9,6 +9,19 @@ public class Buscaminas {
         }
     }
 
+    public static void fillDisplay(String[][] displayBoard){
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                displayBoard[i][j] = '#';
+            }
+        }
+    }
+
+    public static void select(int i, int j){
+        if(i > 10 || i < 1 || j > 10 || j < 1)
+            System.out.println("Please choose valid indexes");
+    }
+    
     public static void run(String [][] board, String [][] displayedBoard){
         fill(board);
         Display.display(board);
