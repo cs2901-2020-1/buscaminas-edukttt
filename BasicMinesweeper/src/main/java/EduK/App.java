a166cd347ae8d58be257078f5073f814a5f35408
StephanoWurttele <stephano.wurttele@utec.edu.pe>
2020-06-04 12:01:22 -0500

    D:

--- BasicMinesweeper/src/main/java/EduK/App.java
+++ BasicMinesweeper/src/main/java/EduK/App.java
@@ -1,13 +1,17 @@
 package EduK;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    static Scanner in;
+    static String[] board;
+    static String turn;
+
+    public static void App(String[] args) {
+        in = new Scanner(System.in);
+        String [][] board = new String[10][10];
+        String [][] displayedBoard = new String[10][10];
+        Buscaminas.run(board, displayedBoard);
+
+        TicTacToe.run(board, turn, in);
     }
 }
